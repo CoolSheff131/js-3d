7611f6651c56d6bc05ed142a08c6992ec8129944
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-10-08 21:56:03 +0800

    Добавлены экраны

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const burger = document.querySelector('.burger')
 const container = document.querySelector('.container')
-
+const screens = document.querySelectorAll('.screen')
 burger.addEventListener('click',()=>{
     container.classList.toggle('active')
 })
